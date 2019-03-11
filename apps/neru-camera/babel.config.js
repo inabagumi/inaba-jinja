db36b582b0d77f92844a2e6f76294557792b932b
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-03-12 01:25:49 +0900

    chore(deps): rm babel-plugin-dynamic-import-node (inabagumi/neru-camera#121)

--- apps/neru-camera/babel.config.js
+++ apps/neru-camera/babel.config.js
@@ -1,14 +1,3 @@
-module.exports = api => {
-  const env = api.env()
-
-  const plugins = []
-
-  if (env === 'test') {
-    plugins.push('dynamic-import-node')
-  }
-
-  return {
-    plugins,
-    presets: ['@vue/app']
-  }
+module.exports = {
+  presets: ['@vue/app']
 }
