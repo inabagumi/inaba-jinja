51c75d23deccf82ded51e0e376f45e49883632ca
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-05-08 21:45:55 +0900

    skip hello (inabagumi/neru-camera#166)

--- apps/neru-camera/src/main.ts
+++ apps/neru-camera/src/main.ts
@@ -1,4 +1,5 @@
 import 'pinch-zoom-element'
+import { utils as pixiUtils } from 'pixi.js'
 import { register } from 'register-service-worker'
 import Vue from 'vue'
 import App from './App.vue'
@@ -11,6 +12,8 @@ if (process.env.NODE_ENV === 'production') {
 
 Vue.config.productionTip = false
 Vue.config.ignoredElements = ['pinch-zoom']
+
+pixiUtils.skipHello()
 
 new Vue({
   render: h => h(App),
