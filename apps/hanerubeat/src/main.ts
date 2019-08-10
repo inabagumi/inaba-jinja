108193701f57aade82e76b800f87fde17a21c17b
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-08-10 10:47:47 +0900

    replace to @inabagumi/eslint-config (inabagumi/hanerubeat#80)

--- apps/hanerubeat/src/main.ts
+++ apps/hanerubeat/src/main.ts
@@ -1,9 +1,9 @@
 import './registerServiceWorker'
-import Vue from 'vue'
+import Vue, { VNode } from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: (h): VNode => h(App)
 }).$mount('#app')
--- apps/hanerubeat/src/registerServiceWorker.ts
+++ apps/hanerubeat/src/registerServiceWorker.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 
-async function register() {
+async function register(): Promise<void> {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     const { Workbox } = await import('workbox-window')
     const wb = new Workbox(`${process.env.BASE_URL}service-worker.js`)
@@ -9,4 +9,6 @@ async function register() {
   }
 }
 
-register().catch(error => console.error(error))
+register().catch((error): void => {
+  console.error(error)
+})
