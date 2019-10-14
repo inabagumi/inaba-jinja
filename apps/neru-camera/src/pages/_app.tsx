02372fcd76cc9d04c6977a18eedb7e4215323512
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-10-14 19:21:30 +0900

    feat: add ogp (inabagumi/neru-camera#311)

--- apps/neru-camera/src/pages/_app.tsx
+++ apps/neru-camera/src/pages/_app.tsx
@@ -1,16 +1,54 @@
-import App from 'next/app'
+import App, { AppContext, AppInitialProps } from 'next/app'
 import Head from 'next/head'
 import React, { ReactElement } from 'react'
 import { AssetProvider } from '../context/asset-context'
 
-export default class extends App {
+type Props = {
+  baseUrl: string
+  description: string
+  title: string
+}
+
+export default class extends App<Props> {
+  public static async getInitialProps(
+    ctx: AppContext
+  ): Promise<AppInitialProps & Props> {
+    const appInitialProps = await super.getInitialProps(ctx)
+
+    return {
+      ...appInitialProps,
+      baseUrl: process.env.NERU_CAMERA_BASE_URL || '',
+      description: process.env.NERU_CAMERA_DESCRIPTION || '',
+      title: process.env.NERU_CAMERA_TITLE || ''
+    }
+  }
+
   public render(): ReactElement {
-    const { Component, pageProps } = this.props
+    const { Component, baseUrl, description, pageProps, title } = this.props
 
     return (
       <AssetProvider>
         <Head>
-          <title>ねるカメラ</title>
+          <title>{title}</title>
+
+          <meta content={description} name="description" />
+
+          <meta content="website" property="og:type" />
+          <meta content={`${baseUrl}/`} property="og:url" />
+          <meta content={title} property="og:title" />
+          <meta content={description} property="og:description" />
+          <meta
+            content={`${baseUrl}/img/main-visual.jpg`}
+            property="og:image"
+          />
+          <meta content="summary_large_image" name="twitter:card" />
+          <meta content={title} name="twitter:title" />
+          <meta content={description} name="twitter:description" />
+          <meta
+            content={`${baseUrl}/img/main-visual.jpg`}
+            name="twitter:image"
+          />
+
           <link
             as="style"
             href="https://fonts.googleapis.com/css?display=swap&amp;family=Roboto:400"
