705f76fda38a2ef0b8e0cd6fcf92be955020c539
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-12-04 16:58:50 +0900

    feat: use templates (#21)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
+import Typography from '@material-ui/core/Typography'
 import Head from 'next/head'
 import React from 'react'
 import { defineMessages, useIntl } from 'react-intl'
+import Layout from '../components/templates/Layout'
 
 const messages = defineMessages({
   title: {
@@ -17,6 +19,10 @@ export default () => {
       <Head>
         <title>{intl.formatMessage(messages.title)}</title>
       </Head>
+
+      <Layout>
+        <Typography variant="body1">home</Typography>
+      </Layout>
     </>
   )
 }
