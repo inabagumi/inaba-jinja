6791bad68f3dad407915af3d5c2b4c347152fbf5
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-12-05 00:41:22 +0900

    feat: add hero (#29)

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,42 +1,29 @@
 import AppBar from '@material-ui/core/AppBar'
+import IconButton from '@material-ui/core/IconButton'
 import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
+import MenuIcon from '@material-ui/icons/Menu'
 import React, { FC } from 'react'
 import { defineMessages, useIntl } from 'react-intl'
-import Logo from '../atoms/Logo'
-
-const useStyles = makeStyles({
-  title: {
-    display: 'flex',
-    flexGrow: 1,
-    justifyContent: 'center'
-  }
-})
 
 const messages = defineMessages({
-  title: {
-    defaultMessage: 'Haneru Jinja',
-    id: 'app.title'
+  openDrawer: {
+    defaultMessage: 'Open drawer',
+    id: 'app.open_drawer'
   }
 })
 
 const Header: FC = () => {
   const intl = useIntl()
-  const classes = useStyles({})
 
   return (
     <AppBar color="default" elevation={0} position="static">
       <Toolbar>
-        <Typography
-          align="center"
+        <IconButton
+          aria-label={intl.formatMessage(messages.openDrawer)}
           color="inherit"
-          className={classes.title}
-          noWrap
-          variant="h6"
         >
-          <Logo aria-label={intl.formatMessage(messages.title)} height="30" />
-        </Typography>
+          <MenuIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   )
