eabb962c8aef09cfbb9eae796df3f61e4c038715
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-12-08 23:01:41 +0900

    feat: improve critical rendering path (#60)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,42 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+
+const merge = require('webpack-merge')
+
 const nextConfig = {
   experimental: {
     plugins: true
-  }
+  },
+  webpack: (config, { dev }) =>
+    merge(config, {
+      module: {
+        rules: [
+          {
+            test: /\.jpe?g$/i,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 8192,
+                  name: dev
+                    ? '[path][name].[ext]?[contenthash:8]'
+                    : '[path][name].[contenthash:8].[ext]',
+                  outputPath: 'static/images',
+                  publicPath: '/_next/static/images'
+                }
+              }
+            ]
+          },
+          {
+            test: /\.svg$/i,
+            use: [
+              {
+                loader: 'react-svg-loader'
+              }
+            ]
+          }
+        ]
+      }
+    })
 }
 
 module.exports = nextConfig
