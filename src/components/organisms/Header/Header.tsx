8bfc0989f7d985183c7360265894dae858bb6b28
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-12-13 01:22:53 +0900

    feat: add error page

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,19 +1,13 @@
 import AppBar from '@material-ui/core/AppBar'
 import Collapse from '@material-ui/core/Collapse'
-import IconButton from '@material-ui/core/IconButton'
+import Container from '@material-ui/core/Container'
+import Link from '@material-ui/core/Link'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import MenuIcon from '@material-ui/icons/Menu'
 import { createStyles, makeStyles } from '@material-ui/core/styles'
+import NextLink from 'next/link'
 import React, { FC } from 'react'
-import { FormattedMessage, defineMessages, useIntl } from 'react-intl'
-
-const messages = defineMessages({
-  openDrawer: {
-    defaultMessage: 'Open drawer',
-    id: 'app.open_drawer'
-  }
-})
+import { FormattedMessage } from 'react-intl'
 
 const useStyles = makeStyles(theme =>
   createStyles({
@@ -31,24 +25,24 @@ type Props = {
 }
 
 const Header: FC<Props> = ({ hide = false }) => {
-  const intl = useIntl()
   const classes = useStyles({})
 
   return (
     <Collapse in={!hide}>
       <AppBar color="default" elevation={0} position="fixed">
         <Toolbar>
-          <IconButton
-            aria-label={intl.formatMessage(messages.openDrawer)}
-            className={classes.menuButton}
-            color="inherit"
-          >
-            <MenuIcon />
-          </IconButton>
-
-          <Typography className={classes.title} noWrap variant="h6">
-            <FormattedMessage defaultMessage="Inaba Jinja" id="app.title" />
-          </Typography>
+          <Container>
+            <Typography className={classes.title} noWrap variant="h6">
+              <NextLink href="/" passHref prefetch={false}>
+                <Link color="inherit" underline="none">
+                  <FormattedMessage
+                    defaultMessage="Inaba Jinja"
+                    id="app.title"
+                  />
+                </Link>
+              </NextLink>
+            </Typography>
+          </Container>
         </Toolbar>
       </AppBar>
       <Toolbar />
