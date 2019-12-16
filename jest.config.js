8b4164bff858187a61ffeb9472e60dcf91f83165
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-12-17 03:10:12 +0900

    chore: remove unnecessary property from jest.config

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,6 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: 'coverage',
   coveragePathIgnorePatterns: ['/.next/', '/node_modules/'],
-  globals: {
-    'ts-jest': {
-      tsConfig: {
-        jsx: 'react'
-      }
-    }
-  },
   moduleNameMapper: {
     '\\.(?:jpe?g|webp)$': '<rootDir>/__mocks__/fileMock.ts',
     '\\.svg$': '<rootDir>/__mocks__/svgMock.ts'
