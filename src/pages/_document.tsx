52b2d28b4afc55241e43371abde8318e0ae80311
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-12-18 21:09:37 +0900

    feat: disable web fonts
    BREAKING CHANGE: Stop using Google Font.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,24 +6,13 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          <link href="https://fonts.gstatic.com" rel="preconnect" />
           <link href="https://www.google-analytics.com" rel="preconnect" />
           <link href="https://www.googletagmanager.com" rel="preconnect" />
-          <link
-            as="style"
-            href="https://fonts.googleapis.com/css?display=swap&amp;family=Roboto:300,400,500,700|Roboto+Slab:300,400,500,700|Noto+Sans+JP:300,400,500,700|Noto+Serif+JP:300,400,500,700"
-            rel="preload"
-          />
         </Head>
         <body>
           <Main />
 
           <NextScript />
-
-          <link
-            href="https://fonts.googleapis.com/css?display=swap&amp;family=Roboto:300,400,500,700|Roboto+Slab:300,400,500,700|Noto+Sans+JP:300,400,500,700|Noto+Serif+JP:300,400,500,700"
-            rel="stylesheet"
-          />
         </body>
       </Html>
     )
