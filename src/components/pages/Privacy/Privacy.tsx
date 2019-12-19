c8f2ab0147bcab101229fca6ab16519bceb8213c
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-12-19 10:51:14 +0900

    feat: add title to privacy page (#161)

---
+++ src/components/pages/Privacy/Privacy.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import Page from '../../../layouts/Main'
+
+const Privacy: FC = () => {
+  return <Page />
+}
+
+export default Privacy
--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,7 +1,26 @@
 import { NextPage } from 'next'
 import React from 'react'
-import Page from '../layouts/Main'
+import { defineMessages, useIntl } from 'react-intl'
+import Meta from '../components/atoms/Meta'
+import ActualPrivacy from '../components/pages/Privacy'
 
-const Privacy: NextPage = () => <Page />
+const messages = defineMessages({
+  title: {
+    defaultMessage: 'プライバシーポリシー',
+    id: 'privacy_policy.title'
+  }
+})
+
+const Privacy: NextPage = () => {
+  const intl = useIntl()
+
+  return (
+    <>
+      <Meta title={intl.formatMessage(messages.title)} />
+
+      <ActualPrivacy />
+    </>
+  )
+}
 
 export default Privacy
