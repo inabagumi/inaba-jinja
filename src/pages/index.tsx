3ce37105d3f7db5c30497199193514184422d8d2
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-01-06 02:42:33 +0900

    feat: add ogp to home (#194)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react'
+import cardImage from '../assets/card.jpg'
 import Meta from '../components/atoms/Meta'
 import Home from '../components/pages/Home'
 
 const HomePage: FC = () => {
   return (
     <>
-      <Meta pathname="/" title="因幡神社" />
+      <Meta image={cardImage} pathname="/" title="因幡神社" />
 
       <Home />
     </>
