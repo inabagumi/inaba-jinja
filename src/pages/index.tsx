4fe8b4f66ba28b360809d36aa65a79f639fefaaa
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-01-07 02:59:58 +0900

    fix: add description to home (#210)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,12 @@ import Home from '../components/pages/Home'
 const HomePage: FC = () => {
   return (
     <>
-      <Meta image={cardImage} pathname="/" title="因幡神社" />
+      <Meta
+        description="因幡神社は東京都北区赤羽のどこかにある神社です。有閑喫茶 あにまーれの因幡はねる様をご祭神としてお祀りしています。"
+        image={cardImage}
+        pathname="/"
+        title="因幡神社"
+      />
 
       <Home />
     </>
