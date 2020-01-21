326a99f27e075ce9b0f1db6121578edd2be091a7
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-01-21 15:14:30 +0900

    fix(lottery): use clearTimeout

---
+++ src/styles/shake.ts
@@ -0,0 +1,13 @@
+import { keyframes } from '@emotion/core'
+
+const shake = keyframes`
+  from {
+    transform: translateY(0) rotate(180deg);
+  }
+
+  to {
+    transform: translateY(20px) rotate(170deg);
+  }
+`
+
+export default shake
