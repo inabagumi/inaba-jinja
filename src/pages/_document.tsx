452f3483147d0fb6e21e5505cd3c30218733476d
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-02-06 07:35:11 +0900

    chore(next): replace to @next/plugin-google-analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -29,26 +29,8 @@ class MyDocument extends Document<EmotionCritical> {
     return (
       <Html lang="ja">
         <Head>
-          {process.env.GA_TRACKING_ID && (
-            <>
-              <link href="https://www.google-analytics.com" rel="preconnect" />
-              <link href="https://www.googletagmanager.com" rel="preconnect" />
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: [
-                    'window.dataLayer = window.dataLayer || [];',
-                    'function gtag(){dataLayer.push(arguments);}',
-                    "gtag('js', new Date());",
-                    `gtag('config', '${process.env.GA_TRACKING_ID}');`
-                  ].join('')
-                }}
-              />
-            </>
-          )}
+          <link href="https://www.google-analytics.com" rel="preconnect" />
+          <link href="https://www.googletagmanager.com" rel="preconnect" />
           <style
             dangerouslySetInnerHTML={{ __html: css }}
             data-emotion-css={ids.join(' ')}
