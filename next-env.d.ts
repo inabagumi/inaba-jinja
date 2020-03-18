c22ffc5a4775ccba3a8d68f181a58977648b4b6b
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-03-19 08:44:11 +0900

    chore: remove @inabagumi/next-images (#433)

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,6 +2,18 @@
 /// <reference types="next/types/global" />
 /// <reference types="@inabagumi/next-images" />
 
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.png'
+declare module '*.webp'
+
+declare module '*.svg' {
+  import { FC, ReactSVGElement, SVGProps } from 'react'
+
+  const content: FC<SVGProps<SVGSVGElement>>
+  export default content
+}
+
 declare module '*.mdx' {
   const MDXComponent: () => JSX.Element
 
