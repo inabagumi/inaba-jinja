a9609828446394c6d914b298d80559132dbebfdb
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-05-06 19:58:40 +0900

    build: remove env from next.config (#614)

--- next.config.js
+++ next.config.js
@@ -14,8 +14,6 @@ const release = [
 
 const nextConfig = {
   env: {
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
-    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     GA_TRACKING_ID: process.env.GA_TRACKING_ID,
     SENTRY_DSN: process.env.SENTRY_DSN,
     SENTRY_RELEASE: release
