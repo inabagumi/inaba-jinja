3996686cf25ae993a7d5acebba6a238e1da06fdf
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-05-15 03:02:41 +0900

    build: replace to styled-jsx (#641)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,40 +1,13 @@
-import { extractCritical } from '@emotion/server'
-import { EmotionCritical } from '@emotion/server/create-instance'
-import Document, {
-  DocumentContext,
-  DocumentInitialProps,
-  Head,
-  Html,
-  Main,
-  NextScript
-} from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 import React from 'react'
 
-class MyDocument extends Document<EmotionCritical> {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps & EmotionCritical> {
-    const initialProps = await super.getInitialProps(ctx)
-    const styles = extractCritical(initialProps.html)
-
-    return {
-      ...initialProps,
-      ...styles
-    }
-  }
-
+class MyDocument extends Document {
   render(): JSX.Element {
-    const { css, ids } = this.props
-
     return (
       <Html lang="ja">
         <Head prefix="og: http://ogp.me/ns#">
           <link href="https://www.google-analytics.com" rel="preconnect" />
           <link href="https://www.googletagmanager.com" rel="preconnect" />
-          <style
-            dangerouslySetInnerHTML={{ __html: css }}
-            data-emotion-css={ids.join(' ')}
-          />
         </Head>
         <body>
           <Main />
