59f06ea602616e4f6d4340fa281f2ad61597d428
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-06-25 01:49:33 +0900

    feat(theme): localize not found page (inabagumi/website#335)

---
+++ website/src/theme/NotFound.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from '@theme/Layout'
+
+const NotFound = () => (
+  <Layout title="ページが見つかりません">
+    <div className="container margin-vert--xl">
+      <div className="row">
+        <div className="col col--6 col--offset-3">
+          <h1 className="hero__title">ページが見つかりません</h1>
+          <p>お探しのページが見つかりませんでした。</p>
+          <p>
+            このページにリンクしているサイトの所有者に連絡をしてリンクが壊れていることを伝えてください。
+          </p>
+        </div>
+      </div>
+    </div>
+  </Layout>
+)
+
+export default NotFound
