4f21e4d374df3985d7e441da3cc66001270d3b57
Yamagishi Kazutoshi <ykzts@haneru.dev>
2020-09-24 22:07:02 +0900

    build(pwa): replace to next-pwa (inabagumi/neru-camera#678)

--- apps/neru-camera/next.config.js
+++ apps/neru-camera/next.config.js
@@ -1,44 +1,58 @@
-const withOffline = require('next-offline')
+const withPWA = require('next-pwa')
 
 const nextConfig = {
   crossOrigin: 'anonymous',
   experimental: {
     plugins: true
   },
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  headers: () => [
-    {
-      headers: [
-        {
-          key: 'cache-control',
-          value: 'max-age=600, s-maxage=60'
-        }
-      ],
-      source: '/((?!_next).*)'
-    },
-    {
-      headers: [
-        {
-          key: 'cache-control',
-          value: 'max-age=0'
-        }
-      ],
-      source: '/service-worker.js'
-    }
-  ],
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  rewrites: () => [
-    {
-      destination: '/_next/static/service-worker.js',
-      source: '/service-worker.js'
-    }
-  ],
-  workboxOpts: {
-    clientsClaim: true,
-    runtimeCaching: [],
-    skipWaiting: true,
-    swDest: 'static/service-worker.js'
+  headers() {
+    return [
+      {
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'max-age=600, s-maxage=60'
+          }
+        ],
+        source: '/((?!_next).*)'
+      },
+      {
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'max-age=0'
+          }
+        ],
+        source: '/service-worker.js'
+      },
+      {
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'immutable, max-age=31536000'
+          }
+        ],
+        source: '/workbox-:hash.js'
+      }
+    ]
+  },
+  pwa: {
+    dest: '.next/static',
+    disable: process.env.NODE_ENV === 'development',
+    sw: '/service-worker.js'
+  },
+  rewrites() {
+    return [
+      {
+        destination: '/_next/static/service-worker.js',
+        source: '/service-worker.js'
+      },
+      {
+        destination: '/_next/static/workbox-:hash.js',
+        source: '/workbox-:hash.js'
+      }
+    ]
   }
 }
 
-module.exports = withOffline(nextConfig)
+module.exports = withPWA(nextConfig)
