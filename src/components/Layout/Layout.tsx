87125d5c009a7e19f8eea8bd82716ead2bc5ea67
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-10-22 22:42:27 +0900

    perf: use image optimizer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,59 +1,120 @@
-import Head from 'next/head'
 import type { FC } from 'react'
 import styled, { createGlobalStyle } from 'styled-components'
 
 import {
-  preSrc as placeholder,
-  src as defaultMainVisual
-} from '@/assets/main-visual.jpg'
+  preSrc as mainVisualPlaceholderForLandscape,
+  src as mainVisualURLForLandscape
+} from '@/assets/main-visual@landscape.jpg'
+import {
+  preSrc as mainVisualPlaceholder,
+  src as mainVisualURL
+} from '@/assets/main-visual@portrait.jpg'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
-const mainVisual = process.env.NEXT_PUBLIC_MAIN_VISUAL_URL ?? defaultMainVisual
+const createMainVisualURL = (
+  width: number,
+  orientation: 'portrait' | 'landscape' = 'portrait'
+): string =>
+  `/_next/image?url=${encodeURIComponent(
+    orientation === 'portrait' ? mainVisualURL : mainVisualURLForLandscape
+  )}&q=80&w=${width || 1200}`
 
 const GlobalStyle = createGlobalStyle`
-  html {
-    background-color: #757575;
-    color: #fff;
+  :root {
+    --ij-default-font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue,
+      Helvetica, Arial, YuGothic, Yu Gothic, sans-serif;
+    --ij-serif-font-family: Garamond, Times New Roman, YuMincho, Yu Mincho,
+      serif;
   }
 
-  body::before {
-    background-image: linear-gradient(rgba(0, 0, 0, 0.54), rgba(0, 0, 0, 0.54)),
-      url('${mainVisual}?auto=compress,format'), url('${placeholder}');
-    background-image: linear-gradient(rgba(0, 0, 0, 0.54), rgba(0, 0, 0, 0.54)),
-      image-set(
-        url('${mainVisual}?auto=compress,format&w=0.5') 1x,
-        url('${mainVisual}?auto=compress,format') 2x
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  html {
+    box-sizing: border-box;
+    background-color: #757575;
+    color: #fff;
+    font-family: var(--ij-default-font-family);
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: none;
+  }
+
+  body {
+    background-attachment: fixed;
+    background-image:
+      linear-gradient(
+        rgba(0, 0, 0, 0.54),
+        rgba(0, 0, 0, 0.54)
       ),
-      url('${placeholder}');
+      url('${createMainVisualURL(768)}'),
+      url('${mainVisualPlaceholder}');
+    background-image:
+      linear-gradient(
+        rgba(0, 0, 0, 0.54),
+        rgba(0, 0, 0, 0.54)
+      ),
+      image-set(
+        url('${createMainVisualURL(320)}') 1x,
+        url('${createMainVisualURL(768)}') 2x,
+      ),
+      url('${mainVisualPlaceholder}');
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
-    bottom: 0;
-    content: '';
-    display: block;
-    left: 0;
-    position: fixed;
-    right: 0;
-    top: 0;
-    z-index: -1;
+    line-height: 2;
+    margin: 0;
+  }
 
-    @media (orientation: portrait) {
-      background-image: linear-gradient(
-          rgba(0, 0, 0, 0.54),
-          rgba(0, 0, 0, 0.54)
-        ),
-        url('${mainVisual}?ar=0.8:1&auto=compress,format&fit=crop'),
-        url('${placeholder}');
-      background-image: linear-gradient(
-          rgba(0, 0, 0, 0.54),
-          rgba(0, 0, 0, 0.54)
-        ),
+  @media (orientation: landscape) {
+    body {
+      background-image:
+        linear-gradient(rgba(0, 0, 0, 0.54), rgba(0, 0, 0, 0.54)),
+        url('${createMainVisualURL(768, 'landscape')}'),
+        url('${mainVisualPlaceholderForLandscape}');
+      background-image:
+        linear-gradient(rgba(0, 0, 0, 0.54), rgba(0, 0, 0, 0.54)),
         image-set(
-          url('${mainVisual}?ar=0.8:1&auto=compress,format&fit=crop&w=0.5') 1x,
-          url('${mainVisual}?ar=0.8:1&auto=compress,format&fit=crop') 2x
+          url('${createMainVisualURL(320, 'landscape')}') 1x,
+          url('${createMainVisualURL(768, 'landscape')}') 2x,
         ),
-        url('${placeholder}');
+        url('${mainVisualPlaceholderForLandscape}');
+    }
+  }
+
+  @media (min-width: 960px) {
+    body {
+      background-image:
+        linear-gradient(rgba(0, 0, 0, 0.54), rgba(0, 0, 0, 0.54)),
+        url('${createMainVisualURL(1200, 'landscape')}'),
+        url('${mainVisualPlaceholderForLandscape}');
+      background-image:
+        linear-gradient(rgba(0, 0, 0, 0.54), rgba(0, 0, 0, 0.54)),
+        image-set(
+          url('${createMainVisualURL(768, 'landscape')}') 1x,
+          url('${createMainVisualURL(1200, 'landscape')}') 2x,
+        ),
+        url('${mainVisualPlaceholderForLandscape}');
+    }
+  }
+
+  @media (min-width: 960px) and (orientation: portrait) {
+    body {
+      background-image:
+        linear-gradient(rgba(0, 0, 0, 0.54), rgba(0, 0, 0, 0.54)),
+        url('${createMainVisualURL(1200)}'),
+        url('${mainVisualPlaceholder}');
+      background-image:
+        linear-gradient(rgba(0, 0, 0, 0.54), rgba(0, 0, 0, 0.54)),
+        image-set(
+          url('${createMainVisualURL(768)}') 1x,
+          url('${createMainVisualURL(1200)}') 2x,
+        ),
+        url('${mainVisualPlaceholder}');
     }
   }
 
@@ -69,7 +130,7 @@ const GlobalStyle = createGlobalStyle`
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 100vh;
   position: relative;
   z-index: 0;
 `
@@ -84,33 +145,6 @@ type Props = {
 
 const Layout: FC<Props> = ({ children, hideHeader = false }) => (
   <>
-    <Head>
-      <link
-        as="image"
-        href={`${mainVisual}?auto=compress,format`}
-        media="(orientation: landscape) and (min-resolution: 1.1dppx)"
-        rel="preload"
-      />
-      <link
-        as="image"
-        href={`${mainVisual}?auto=compress,format&w=0.5`}
-        media="(orientation: landscape) and (max-resolution: 1dppx)"
-        rel="preload"
-      />
-      <link
-        as="image"
-        href={`${mainVisual}?ar=0.8:1&auto=compress,format&fit=crop`}
-        media="(orientation: portrait) and (min-resolution: 1.1dppx)"
-        rel="preload"
-      />
-      <link
-        as="image"
-        href={`${mainVisual}?ar=0.8:1&auto=compress,format&fit=crop&w=2x`}
-        media="(orientation: portrait) and (max-resolution: 1dppx)"
-        rel="preload"
-      />
-    </Head>
-
     <GlobalStyle />
 
     <Wrapper>
