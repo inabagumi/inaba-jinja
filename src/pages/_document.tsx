c5e8fd86a04a28201f5ced273953a0439eb179ed
Yamagishi Kazutoshi <ykzts@haneru.dev>
2020-10-28 05:03:57 +0900

    feat(app): replace to ga4 (#1003)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -65,6 +65,25 @@ class Document extends NextDocument {
             rel="apple-touch-icon"
             sizes="152x152"
           />
+
+          {process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: [
+                    'window.dataLayer = window.dataLayer || [];',
+                    'function gtag(){dataLayer.push(arguments);}',
+                    "gtag('js', new Date());",
+                    `gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');`
+                  ].join('')
+                }}
+              />
+            </>
+          )}
         </Head>
 
         <body>
