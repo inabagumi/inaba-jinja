5ef94e1b315e659cb13541059ac6c2cc523d3d9b
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-11-13 02:08:26 +0900

    fix(layout): change bg color on skip nav

--- src/components/skip-nav.tsx
+++ src/components/skip-nav.tsx
@@ -15,18 +15,20 @@ const StyledSkipNavLink = styled(SkipNavLinkImpl)`
   /**
    * see https://github.com/reach/reach-ui/blob/v0.11.2/packages/skip-nav/styles.css
    */
-
   border: 0;
   clip: rect(0 0 0 0);
+  color: #fff;
   height: 1px;
+  line-height: 1;
   margin: -1px;
   overflow: hidden;
   padding: 0;
   position: absolute;
+  text-decoration: none;
   width: 1px;
 
   :focus {
-    background-color: #fff;
+    background-color: var(--ij-color-primary);
     clip: auto;
     height: auto;
     left: 10px;
