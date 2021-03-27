ce3199fd52adb3f67f0f272593ecb65b862d6e8a
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-03-28 01:17:57 +0900

    ci(vercel): use commit message (#1300)

--- scripts/check-build-step.sh
+++ scripts/check-build-step.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "VERCEL_ENV: $VERCEL_ENV"
-echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
 
-if [[ "$VERCEL_ENV" != "production" || "$VERCEL_GIT_COMMIT_REF" =~ ^v[[:digit:]]+(\.[[:digit:]]+(\.[[:digit:]]+)?)?$ ]] ; then
+if [[ "$VERCEL_ENV" != "production" || "$VERCEL_GIT_COMMIT_MESSAGE" =~ ^chore\(release\): ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1
