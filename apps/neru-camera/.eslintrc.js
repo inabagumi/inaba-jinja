49261c75d75f96b911ff598782ca3d862d31e3ff
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-05-15 14:50:36 +0900

    ci(eslint): extract eslintrc (inabagumi/neru-camera#951)

---
+++ apps/neru-camera/.eslintrc.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('eslint/lib/shared/types').ConfigData}
+ */
+module.exports = {
+  extends: ['@inabagumi/react'],
+  parserOptions: {
+    project: 'tsconfig.json'
+  },
+  root: true
+}
