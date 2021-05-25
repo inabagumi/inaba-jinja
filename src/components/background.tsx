b4d4222858e1816db85281ef9db765da86b254e4
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-05-26 02:36:00 +0900

    refactor: enable blurry placeholder (#1414)

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -2,17 +2,24 @@ import Image from 'next/image'
 import type { VFC } from 'react'
 
 import mainVisual from '@/assets/main-visual.jpg'
-import Placeholder from '@/components/placeholder'
+import useBlurDataURL from '@/hooks/use-blur-data-url'
 import styles from '@/styles/components/background.module.css'
 
 const Background: VFC = () => {
+  const blurDataURL = useBlurDataURL(
+    mainVisual.placeholder,
+    mainVisual.width,
+    mainVisual.height
+  )
+
   return (
     <div aria-hidden className={styles.container} role="none presentation">
-      <Placeholder src={mainVisual.placeholder} />
       <Image
         alt=""
+        blurDataURL={blurDataURL}
         layout="fill"
         objectFit="cover"
+        placeholder="blur"
         priority
         quality={70}
         src={mainVisual.src}
