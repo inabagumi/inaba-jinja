c492df8f9493cea62a93020f804ef5d72afea3d1
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-10-31 22:07:43 +0900

    feat(style): always showing footer (#1715)

--- components/link.tsx
+++ components/link.tsx
@@ -4,15 +4,21 @@ import type { ComponentPropsWithoutRef } from 'react'
 
 export type Props = ComponentPropsWithoutRef<'a'>
 
-const Link = forwardRef<HTMLAnchorElement, Props>(function Link({
-  href = '',
-  ...props
-}) {
+const Link = forwardRef<HTMLAnchorElement, Props>(function Link(
+  { href = '', ...props },
+  ref
+) {
   return /^https?:\/\//i.test(href) ? (
-    <a href={href} rel="noopener noreferrer" target="_blank" {...props} />
+    <a
+      href={href}
+      rel="noopener noreferrer"
+      target="_blank"
+      {...props}
+      ref={ref}
+    />
   ) : (
     <NextLink href={href}>
-      <a {...props} />
+      <a {...props} ref={ref} />
     </NextLink>
   )
 })
