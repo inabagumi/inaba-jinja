2ede6c802f9cfbd15f36287c26230ed3b8693d87
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-10-31 21:55:39 +0900

    feat(style): always showing footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -28,118 +28,116 @@ const Layout: VFC<Props> = ({ children, hideHeader = false }) => {
   }, [])
 
   return (
-    <>
+    <div className={styles.wrapper}>
       <SkipNavLink className={styles.skipNavLink}>
         コンテンツにスキップ
       </SkipNavLink>
 
-      <div className={styles.wrapper}>
-        <IconButton
-          aria-controls="global-menu"
-          aria-expanded={menuShown}
-          aria-label="メニューを開く"
-          className={styles.menuButton}
-          onClick={showMenu}
-          type="button"
+      <IconButton
+        aria-controls="global-menu"
+        aria-expanded={menuShown}
+        aria-label="メニューを開く"
+        className={styles.menuButton}
+        onClick={showMenu}
+        type="button"
+      >
+        <span aria-hidden className={styles.menuButtonBar} />
+        <span aria-hidden className={styles.menuButtonBar} />
+        <span aria-hidden className={styles.menuButtonBar} />
+      </IconButton>
+      <div
+        aria-hidden
+        className={clsx(styles.backdrop, {
+          [styles.backdropShow]: menuShown
+        })}
+        onClick={hideMenu}
+        role="none presentation"
+        tabIndex={-1}
+      />
+      <RemoveScroll allowPinchZoom enabled={menuShown} forwardProps>
+        <FocusLock
+          as="nav"
+          className={styles.menu}
+          disabled={!menuShown}
+          lockProps={{
+            'aria-hidden': !menuShown,
+            id: 'global-menu'
+          }}
+          noFocusGuards
+          returnFocus
         >
-          <span aria-hidden className={styles.menuButtonBar} />
-          <span aria-hidden className={styles.menuButtonBar} />
-          <span aria-hidden className={styles.menuButtonBar} />
-        </IconButton>
-        <div
-          aria-hidden
-          className={clsx(styles.backdrop, {
-            [styles.backdropShow]: menuShown
-          })}
-          onClick={hideMenu}
-          role="none presentation"
-          tabIndex={-1}
-        />
-        <RemoveScroll allowPinchZoom enabled={menuShown} forwardProps>
-          <FocusLock
-            as="nav"
-            className={styles.menu}
-            disabled={!menuShown}
-            lockProps={{
-              'aria-hidden': !menuShown,
-              id: 'global-menu'
-            }}
-            noFocusGuards
-            returnFocus
-          >
-            <header className={styles.menuHeader}>
-              <Link href="/" prefetch={false}>
-                <a
-                  className={styles.menuLogoLink}
-                  onClick={hideMenu}
-                  onKeyPress={hideMenu}
-                  tabIndex={!menuShown ? -1 : undefined}
-                >
-                  <Logo aria-label="因幡神社" className={styles.menuLogo} />
-                </a>
-              </Link>
-            </header>
-
-            <div className={styles.menuContent} />
-
-            <footer>
-              <ul className={styles.menuList} role="menu">
-                <li className={styles.menuItem} role="menuitem">
-                  <Link href="/about" prefetch={false}>
-                    <a
-                      className={styles.menuLink}
-                      href="/about"
-                      onClick={hideMenu}
-                      onKeyPress={hideMenu}
-                      tabIndex={!menuShown ? -1 : undefined}
-                    >
-                      因幡神社とは
-                    </a>
-                  </Link>
-                </li>
-                <li className={styles.menuItem} role="menuitem">
-                  <Link href="/privacy" prefetch={false}>
-                    <a
-                      className={styles.menuLink}
-                      href="/privacy"
-                      onClick={hideMenu}
-                      onKeyPress={hideMenu}
-                      tabIndex={!menuShown ? -1 : undefined}
-                    >
-                      プライバシーポリシー
-                    </a>
-                  </Link>
-                </li>
-                <li className={styles.menuItem} role="menuitem">
-                  <a
-                    className={styles.menuLink}
-                    href="https://haneru.dev"
-                    onClick={hideMenu}
-                    onKeyPress={hideMenu}
-                    rel="noopener noreferrer"
-                    tabIndex={!menuShown ? -1 : undefined}
-                    target="_blank"
-                  >
-                    運営者情報
-                  </a>
-                </li>
-              </ul>
-            </footer>
-          </FocusLock>
-        </RemoveScroll>
-
-        {!hideHeader && (
-          <header className={styles.header}>
+          <header className={styles.menuHeader}>
             <Link href="/" prefetch={false}>
-              <a className={styles.brand}>
-                <Logo aria-label="因幡神社" className={styles.brandLogo} />
+              <a
+                className={styles.menuLogoLink}
+                onClick={hideMenu}
+                onKeyPress={hideMenu}
+                tabIndex={!menuShown ? -1 : undefined}
+              >
+                <Logo aria-label="因幡神社" className={styles.menuLogo} />
               </a>
             </Link>
           </header>
-        )}
 
-        {children}
-      </div>
+          <div className={styles.menuContent} />
+
+          <footer>
+            <ul className={styles.menuList} role="menu">
+              <li className={styles.menuItem} role="menuitem">
+                <Link href="/about" prefetch={false}>
+                  <a
+                    className={styles.menuLink}
+                    href="/about"
+                    onClick={hideMenu}
+                    onKeyPress={hideMenu}
+                    tabIndex={!menuShown ? -1 : undefined}
+                  >
+                    因幡神社とは
+                  </a>
+                </Link>
+              </li>
+              <li className={styles.menuItem} role="menuitem">
+                <Link href="/privacy" prefetch={false}>
+                  <a
+                    className={styles.menuLink}
+                    href="/privacy"
+                    onClick={hideMenu}
+                    onKeyPress={hideMenu}
+                    tabIndex={!menuShown ? -1 : undefined}
+                  >
+                    プライバシーポリシー
+                  </a>
+                </Link>
+              </li>
+              <li className={styles.menuItem} role="menuitem">
+                <a
+                  className={styles.menuLink}
+                  href="https://haneru.dev"
+                  onClick={hideMenu}
+                  onKeyPress={hideMenu}
+                  rel="noopener noreferrer"
+                  tabIndex={!menuShown ? -1 : undefined}
+                  target="_blank"
+                >
+                  運営者情報
+                </a>
+              </li>
+            </ul>
+          </footer>
+        </FocusLock>
+      </RemoveScroll>
+
+      {!hideHeader && (
+        <header className={styles.header}>
+          <Link href="/" prefetch={false}>
+            <a className={styles.brand}>
+              <Logo aria-label="因幡神社" className={styles.brandLogo} />
+            </a>
+          </Link>
+        </header>
+      )}
+
+      <div className={styles.content}>{children}</div>
 
       <footer className={styles.footer}>
         <nav>
@@ -171,7 +169,7 @@ const Layout: VFC<Props> = ({ children, hideHeader = false }) => {
           </ul>
         </nav>
       </footer>
-    </>
+    </div>
   )
 }
 
