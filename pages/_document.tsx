a9d02d8d69e590c50fd2311d3f00b5d8e285a5fd
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-11-22 20:54:15 +0900

    fix(document): replace to class based component (#1764)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,23 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 import type { VFC } from 'react'
 
-const Document: VFC = () => {
-  return (
-    <Html>
-      <Head prefix="og: http://ogp.me/ns#">
-        <meta content="#ff5722" name="theme-color" />
-        <link href="/manifest.webmanifest" rel="manifest" />
-      </Head>
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head prefix="og: http://ogp.me/ns#">
+          <meta content="#ff5722" name="theme-color" />
+          <link href="/manifest.webmanifest" rel="manifest" />
+        </Head>
 
-      <body>
-        <Main />
+        <body>
+          <Main />
 
-        <NextScript />
-      </body>
-    </Html>
-  )
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
-export default Document
+export default MyDocument
