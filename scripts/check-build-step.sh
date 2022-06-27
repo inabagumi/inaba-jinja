8c11eb1eef570b466e563377c55c5d63a7ef946d
Yamagishi Kazutoshi <ykzts@desire.sh>
2022-06-27 11:36:30 +0900

    fix: follow newer release commit messeage

--- scripts/check-build-step.sh
+++ scripts/check-build-step.sh
@@ -3,7 +3,7 @@
 echo "VERCEL_ENV: $VERCEL_ENV"
 echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
 
-if [[ "$VERCEL_ENV" != "production" || "$VERCEL_GIT_COMMIT_MESSAGE" =~ ^chore:[^\S]+release[^\S]+[0-9]+\.[0-9]+\.[0-9]+[^\S]+\(\#[0-9]+\) ]] ; then
+if [[ "$VERCEL_ENV" != "production" || "$VERCEL_GIT_COMMIT_MESSAGE" =~ ^chore[^:]*:[^\S]+release[^\S]+[0-9]+\.[0-9]+\.[0-9]+[^\S]+\(\#[0-9]+\) ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1
