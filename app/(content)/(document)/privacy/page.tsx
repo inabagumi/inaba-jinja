3158e40adf39a7a547e046e2cff6524bea6ce142
Yamagishi Kazutoshi <ykzts@haneru.dev>
2023-03-13 21:23:02 +0900

    fix: disable edge runtime (#2547)

--- app/(content)/(document)/privacy/page.tsx
+++ app/(content)/(document)/privacy/page.tsx
@@ -5,7 +5,7 @@ import Privacy from './Privacy.mdx'
 
 const title = 'プライバシーポリシー'
 
-export const runtime = 'edge'
+// export const runtime = 'edge'
 export const dynamic = 'force-static'
 
 export const metadata: Metadata = {
