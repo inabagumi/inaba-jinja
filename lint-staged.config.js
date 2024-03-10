088deb65493fc9646f6a324b503ac9b4a5f4ec37
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-03-11 00:05:45 +0900

    build(lint-staged): add css

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 /** @type {import('lint-staged').Config} */
 const lintstagedrc = {
-  '*.{js,json,ts,tsx,md,mdx,yml}': 'prettier -w'
+  '*.{css,js,json,ts,tsx,md,mdx,yml}': 'prettier -w'
 }
 
 export default lintstagedrc
