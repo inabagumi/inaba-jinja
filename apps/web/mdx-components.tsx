786332b712038d3102bec43a350a4542a5187cfe
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-03-10 17:05:01 +0000

    fix(web)!: disable mdxRs

--- apps/web/mdx-components.tsx
+++ apps/web/mdx-components.tsx
@@ -1,8 +1,5 @@
 import { type MDXComponents } from 'mdx/types'
-import { MarkdownLink } from '@/components/markdown'
 
-export function useMDXComponents(): MDXComponents {
-  return {
-    a: MarkdownLink
-  }
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return components
 }
